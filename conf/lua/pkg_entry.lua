cc95b495773a2cbf73c1f3ac5e5a4474daed6f60
Gethin Zhang <zgxcassar@gmail.com>
2016-09-23 15:26:49 +0800

    add pkg hanle

---
+++ conf/lua/pkg_entry.lua
@@ -0,0 +1,28 @@
+local method = ngx.var.request_method
+local path = ngx.var.uri
+
+ngx.req.read_body()
+
+if method == "GET" then
+    res = ngx.location.capture(
+        "/r1" .. path
+    )
+
+    if res.status ~= 200 then
+        ngx.log(ngx.WARN, "request [" .. path .. "] error :" .. res.status)
+        ngx.exit(res.status)
+    else
+        local mime = res.header["Content-Type"]
+        if not mime then
+            mime = "application/zip"
+        end
+
+        data = res.body
+        ngx.header.content_type = mime
+        ngx.header["Content-Length"] = data:len()
+        ngx.say(data)
+        ngx.exit(ngx.HTTP_OK)
+    end
+else
+    ngx.exit(ngx.HTTP_NOT_ALLOWED)
+end
